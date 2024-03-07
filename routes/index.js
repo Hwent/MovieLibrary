b26c6d0710fa055f8ccfc2aab90fe2eb12002fae
Wentao <wentaowilliam@outlook.com>
2024-03-07 14:48:15 -0500

    Add sidebar

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,21 @@
-var express = require("express");
-var router = express.Router();
+const express = require("express");
+const router = express.Router();
 const indexController = require("../controllers/indexController");
+const movieController = require("../controllers/movieController");
+const genreController = require("../controllers/genreController");
 /* GET home page. */
 router.get("/", indexController.index);
+
+// GET request for list of all movies
+router.get("/movies", movieController.getMovies);
+
+// GET request for one movie
+router.get("/movie/:id", movieController.getMovie);
+
+// GET request for list of all genres
+router.get("/genres", genreController.getGenres);
+
+// GET request for one genre
+router.get("/genre/:id", genreController.getGenre);
 
 module.exports = router;
