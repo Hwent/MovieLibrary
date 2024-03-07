d76d8f1d97658e4bdbfd83c1d9ead38f4a279a08
Wentao <wentaowilliam@outlook.com>
2024-03-07 16:23:37 -0500

    Add genre list and detail page

--- controllers/genreController.js
+++ controllers/genreController.js
@@ -1,14 +1,20 @@
 const asyncHandler = require("express-async-handler");
-const genreModel = require("../models/movie");
+const genreModel = require("../models/genre");
 
 const getGenres = asyncHandler(async (req, res) => {
-  const genres = await genreModel.find();
-  res.json(genres);
+  const genres = await genreModel.find({});
+  if (genres) {
+    res.render("genre_list", { title: "Genre list", genres: genres });
+  }
+  res.status(404);
 });
 
 const getGenre = asyncHandler(async (req, res) => {
   const genre = await genreModel.findById(req.params.id);
-  res.json(genre);
+  if (genre) {
+    res.render("genre_detail", { title: "Genre detail", genre: genre });
+  }
+  res.status(404);
 });
 
 module.exports = {
