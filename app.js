7e075c29bb693607c4cf75e17e15dd0b4504fbc8
Wentao <wentaowilliam@outlook.com>
2024-03-12 00:05:47 -0400

    Before deploy

--- app.js
+++ app.js
@@ -7,6 +7,9 @@ const logger = require("morgan");
 const indexRouter = require("./routes/index");
 const usersRouter = require("./routes/users");
 
+const compression = require("compression");
+const helmet = require("helmet");
+
 const app = express();
 
 // Set up mongoose connection
@@ -33,6 +36,8 @@ app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(compression());
+app.use(helmet());
 app.use(express.static(path.join(__dirname, "public")));
 app.use(limiter);
 app.use("/", indexRouter);
