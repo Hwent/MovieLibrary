d022ad9d41308fe9de6b635951e251a3a6e514a6
Wentao <wentaowilliam@outlook.com>
2024-03-13 22:49:03 -0400

    Add genre detail

--- controllers/genreController.js
+++ controllers/genreController.js
@@ -1,6 +1,6 @@
 const asyncHandler = require("express-async-handler");
 const genreModel = require("../models/genre");
-
+const movieModel = require("../models/movie");
 const getGenres = asyncHandler(async (req, res) => {
   const genres = await genreModel.find({});
   if (genres) {
@@ -10,8 +10,13 @@ const getGenres = asyncHandler(async (req, res) => {
 
 const getGenre = asyncHandler(async (req, res) => {
   const genre = await genreModel.findById(req.params.id);
+  const movies = await movieModel.find({ genre_ids: req.params.id });
   if (genre) {
-    res.render("genre_detail", { title: "Genre detail", genre: genre });
+    res.render("genre_detail", {
+      title: "Genre detail",
+      genre: genre,
+      movies: movies,
+    });
   }
 });
 
